9dd236041cec932806609a642268c1e227cbef69
rtkenny <fh5t4958t@gmail.com>
2023-04-11 10:18:28 +0800

    implementing my muzero

--- muzero_sp.py
+++ muzero_sp.py
@@ -1,3 +1,36 @@
 import replay_buffer_sp
 import trainer_sp
 import self_play_sp
+import models
+
+import numpy
+import torch
+
+from games import tictactoe
+
+if __name__ == "__main__":
+    buffer = {}
+    Game = tictactoe.Game
+    config = tictactoe.MuZeroConfig()
+    checkpoint = {
+        "weights": None,
+        "optimizer_state": None,
+        "total_reward": 0,
+        "muzero_reward": 0,
+        "opponent_reward": 0,
+        "episode_length": 0,
+        "mean_value": 0,
+        "training_step": 0,
+        "lr": 0,
+        "total_loss": 0,
+        "value_loss": 0,
+        "reward_loss": 0,
+        "policy_loss": 0,
+        "num_played_games": 0,
+        "num_played_steps": 0,
+        "num_reanalysed_games": 0,
+        "terminate": False,
+    }
+    model = models.MuZeroNetwork(config)
+    weigths = model.get_weights()
+    checkpoint["weights"] = weigths
